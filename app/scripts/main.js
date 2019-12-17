ad85b5b9a7aad572167a6659a01a7e592316bd86
AhmedElaguab <elaguabahmed97@gmail.com>
2019-12-17 19:08:47 +0100

    feat: handle sidebar submenus

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,11 +2,30 @@ $(function() {
   // 01. Handle sidebar submenus
   const menuLinks = $('.sidebar__menu-link');
   menuLinks.on('click', function() {
-    $(this).toggleClass('active');
-    $(this)
+    const clickedItem = $(this);
+    clickedItem.toggleClass('active');
+    clickedItem
       .parent('.sidebar__menu-item')
       .siblings()
       .children('.sidebar__menu-link')
+      .removeClass('active');
+  });
+
+  const subMenuLinks = $('.sidebar__submenu-link');
+  subMenuLinks.on('click', function() {
+    const clickedItem = $(this);
+
+    clickedItem.toggleClass('active');
+    clickedItem
+      .parent('.sidebar__submenu-item')
+      .siblings()
+      .children()
+      .removeClass('active');
+
+    clickedItem
+      .parents('.sidebar__menu-item')
+      .siblings()
+      .find('.sidebar__submenu-link')
       .removeClass('active');
   });
 });
