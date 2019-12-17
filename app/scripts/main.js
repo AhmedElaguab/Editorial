8e820857f7367aa99e008d8c98efbc7bcd37e616
AhmedElaguab <elaguabahmed97@gmail.com>
2019-12-17 16:36:15 +0100

    feat: add sidebar menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,12 @@
-console.log('\'Allo \'Allo!');
-
+$(function() {
+  // 01. Handle sidebar submenus
+  const menuLinks = $('.sidebar__menu-link');
+  menuLinks.on('click', function() {
+    $(this).toggleClass('active');
+    $(this)
+      .parent('.sidebar__menu-item')
+      .siblings()
+      .children('.sidebar__menu-link')
+      .removeClass('active');
+  });
+});
